2543fe2341c04bf55edb4febaa0ea5b9a6da4f69
yangzhenb <yz3201117@163.com>
2019-05-15 08:00:14 +0800

    添加bootstrap完整包

--- src/components/ArticleDescribe.vue
+++ src/components/ArticleDescribe.vue
@@ -1,22 +1,24 @@
 <template>
     <div>
-        <div style="float:left;width:60%;">
+        <div style="float:left;width:60%">
             <a href="#">
-                <img src="../../static/image/1.gif" alt="">
+                <img :src="article.imgsrc" alt="">
             </a>
         </div>
         <div style="float:right;width:40%">
-            <h4>描述</h4>
-            <p>冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市</p>
+            <h4 class="text-primary ">{{article.title}}</h4>
+            <p style="font-size:14px"><span class="glyphicon glyphicon-calendar"></span> 2019-05-10 周五 22:09:00</p>
+            <p style="font-size:14px">{{article.desc}}</p>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    props: {
+        article: Object
+    }
 }
 </script>
 <style>
-
 </style>
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,6 @@
 <template>
 <div>
-         <ArticleDes></ArticleDes> 
-         <ArticleDes></ArticleDes> 
-         <ArticleDes></ArticleDes> 
-         <ArticleDes></ArticleDes> 
+         <ArticleDes v-for="(article,index) in articles" :key="index" :article="article"></ArticleDes> 
 </div>
 </template>
 <script>
@@ -11,7 +8,68 @@ import ArticleDes from '../components/ArticleDescribe'
 export default {
   data(){
     return {
-      value: ''
+      articles: [
+        {
+          title: '标题1',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/1.gif'
+        },
+        {
+          title: '标题2',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_01.png'
+        },
+        {
+          title: '标题3',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_02.png'
+        },
+        {
+          title: '标题4',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_03.png'
+        },
+        {
+          title: '标题5',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_04.png'
+        },
+        {
+          title: '标题6',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_05.png'
+        },
+        {
+          title: '标题7',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_06.png'
+        },
+        {
+          title: '标题8',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_07.png'
+        },
+        {
+          title: '标题9',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_08.png'
+        },
+        {
+          title: '标题10',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_09.png'
+        },
+        {
+          title: '标题11',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_10.png'
+        },
+        {
+          title: '标题12',
+          desc: '冰河世纪是一个很好看的卡通电影，由迪士尼公司拍摄上市',
+          imgsrc: '../../static/image/blog/blog_01.png'
+        }
+      ]
     }
   },
   components: {
